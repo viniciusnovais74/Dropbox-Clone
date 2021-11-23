f041db066d788375a53b688fabe1ba7a737a03ac
Vinicius Novais <viniciusnovais74@gmail.com>
2021-11-23 15:27:13 -0300

    Atualizando Projeto DropBox

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var formidable = require('formidable');
-var faribase = require('firebase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
